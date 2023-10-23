caaa69e9d6493768e81b7de5df77d197205b49bd
quentchecker <83173149+quentchecker@users.noreply.github.com>
2023-10-23 05:20:11 +0200

    Big update
    - website update 
    - gaymode
    - Quent new agreement
    - kets first agreement (🥳)
    - new body counter

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ setInterval(() => {
     progressBar.style.setProperty('--width', widht + .1)
 }, 5)
 
+
+//                 All countdowns
  let daysItem = document.querySelector("#days")
  let hoursItem = document.querySelector("#hours")
  let minItem = document.querySelector("#min")
@@ -30,7 +32,92 @@ setInterval(() => {
     minItem.innerHTML = min
     secItem.innerHTML = sec
   }
-
   countDown()
-
   setInterval(countDown, 1000)
+
+  
+//countdown Quent
+ let days2Item = document.querySelector("#days2")
+ let hours2Item = document.querySelector("#hours2")
+ let min2Item = document.querySelector("#min2")
+ let sec2Item = document.querySelector("#sec2")
+
+  let countDown2 = () => {
+    let futureDate2 = new Date()
+    let currentDate2 = new Date("9 Jul 2024");
+    let myDate2 = currentDate2 - futureDate2;
+    //console.log(myDate);
+
+    let days2 = Math.floor(myDate2 / 1000 / 60 / 60 / 24);
+    let hours2 = Math.floor(myDate2 / 1000 /60 /60) % 24;
+    let min2 = Math.floor(myDate2 / 1000 /60) % 60;
+    let sec2 = Math.floor(myDate2 / 1000) %60;
+
+
+    days2Item.innerHTML = days2
+    hours2Item.innerHTML = hours2
+    min2Item.innerHTML = min2
+    sec2Item.innerHTML = sec2
+  }
+  countDown2()
+  setInterval(countDown2, 1000)
+
+
+//countdown Kets
+let days3Item = document.querySelector("#days3")
+let hours3Item = document.querySelector("#hours3")
+let min3Item = document.querySelector("#min3")
+let sec3Item = document.querySelector("#sec3")
+
+ let countDown3 = () => {
+   let futureDate3 = new Date()
+   let currentDate3 = new Date("9 Jul 2025");
+   let myDate3 = currentDate3 - futureDate3;
+   //console.log(myDate);
+
+   let days3 = Math.floor(myDate3 / 1000 / 60 / 60 / 24);
+   let hours3 = Math.floor(myDate3 / 1000 /60 /60) % 24;
+   let min3 = Math.floor(myDate3 / 1000 /60) % 60;
+   let sec3 = Math.floor(myDate3 / 1000) %60;
+
+
+   days3Item.innerHTML = days3
+   hours3Item.innerHTML = hours3
+   min3Item.innerHTML = min3
+   sec3Item.innerHTML = sec3
+ }
+ countDown3()
+ setInterval(countDown3, 1000)
+
+
+//bodycount quent
+
+
+
+//var countUpDate = new Date("12 act 2025 00:00:00").getTime();
+//var x = setInterval(funtion(){
+//      var: now = new Date().getTime();
+//      var: distance = countUpDate - now;
+//      var: seconds = Math.floor(myDate2 / 1000) %60;
+//
+//      document.getElementById("Bcount").innerHTML = seconds;
+//},1000);
+
+
+
+//                   gaymode switch
+function switchModeNor(){
+  if(document.getElementById('switchModeNor').checked="false"){
+      document.getElementById('switchModeNor').checked = 0;
+      setTimeout(()=>{window. location = 'index.html' },500);
+      return true;
+  }
+}
+
+function switchModeGay(){
+  if(document.getElementById('switchModeGay').checked){
+      setTimeout(()=>{window. location = 'gaymode.html' },500);
+      return true;
+  }
+  return true;
+}
